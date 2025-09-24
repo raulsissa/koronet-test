5ddd5eb8e83d9e54f02f48d78c508fbf15f7d24a
Raul Sisa <raulsisa@Rauls-MacBook-Pro.local>
2025-09-23 22:16:19 -0500

    modify module path

--- server/index.test.js
+++ server/index.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('./index');
 
 describe('GET /', () => {
   it('responds with Hi Koronet Team.', async () => {
